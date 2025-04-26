81b9422afc0f6c2160a829426dcce2a3ffea5872
qweutqq <grusevojegor@gmail.com>
2025-04-26 14:49:43 +0200

    First update

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ def load_tasks():
         print('No saved files found')       
 def add_task():
     new_task = input("Enter a task: ")
+    if not new_task:
+        print('Task cannot be empty')
+        return
+    for task in todo_list_tasks:
+        if task['title'] == new_task:
+            print('Task already exists')
+            return
     new_task = {"title": new_task, "done": False}
     todo_list_tasks.append(new_task)
     print(f"Task '{new_task['title']}' added to the list.")
@@ -33,50 +40,98 @@ def display_tasks():
 
 
 def remove_task():
-    display_tasks()
+    if todo_list_tasks == []:
+        print('No tasks to remove')
+        return
     remove = 0
+    display_tasks()
     while remove < 1 or remove > len(todo_list_tasks):
-        remove = int(input('Enter a number of task to remove:'))
+        try:
+            remove = int(input('Enter a number of task to remove:'))
+        except ValueError:
+            print('Invalid number, please try again.')
+            continue
         if remove < 1 or remove > len(todo_list_tasks):
             print ("Invalid task number")
     todo_list_tasks.pop(remove-1)
     display_tasks()
 
 def mark_done():
+    if todo_list_tasks == []:
+        print('No tasks to mark as done')
+        return
     display_tasks()
     mark = 0
     while mark < 1 or mark > len(todo_list_tasks):
-        mark = int(input('Enter a number of task to mark as done:'))
+        try:
+            mark = int(input('Enter a number of task to mark as done:'))
+        except ValueError:
+            print('Invalid number, please try again.')
+            continue
         if mark < 1 or mark > len(todo_list_tasks):
             print ("Invalid task number")
+        if todo_list_tasks[mark-1]['done']:
+            print('Task already marked as done')
+            mark = 0
+            continue   
     todo_list_tasks[mark-1]['done'] = True
     display_tasks()
 
-load_tasks()
-print('\nWelcome to the menu, please choose an option:\n')
-print('1 - Add a task\n2 - Display tasks\n3 - Remove a task\n4 - Mark task as done\n5 - Exit')      
-while True:
-    try:
-        choice = int(input('Select an option: '))
-    except ValueError:
-        print('Invalid number, please try again.')
-        continue
-    if choice == 1:
-        add_task()
-        save_tasks()
-    elif choice == 2:
-        display_tasks()
-    elif choice == 3:
-        remove_task()
-        save_tasks()
-    elif choice == 4:
-        mark_done()
-        save_tasks()
-    elif choice == 5:
-        save_tasks()
-        print('Exiting the programm...')
-        break
-    else:
-        if choice < 1 or choice > 5:
+def edit_task():
+    if todo_list_tasks == []:
+        print('No tasks to edit')
+        return
+    display_tasks()
+    edit = 0
+    while edit < 1 or edit > len(todo_list_tasks):
+        try:
+            edit = int(input('Enter a number of task to edit:'))
+        except ValueError:
             print('Invalid number, please try again.')
             continue
+        if edit < 1 or edit > len(todo_list_tasks):
+            print ("Invalid task number")
+    new_task = input("Enter a new task: ")
+    if not new_task:
+        print('Task cannot be empty')
+        return
+    for task in todo_list_tasks:
+        if task['title'] == new_task:
+            print('Task with this name already exists')
+            return
+    todo_list_tasks[edit-1]['title'] = new_task
+    display_tasks() 
+def show_menu():
+    print('1 - Add a task\n2 - Display tasks\n3 - Remove a task\n4 - Mark task as done\n5 - Edit a task\n6 - Exit')      
+    while True:
+        try:
+            choice = int(input('Select an option: '))
+        except ValueError:
+            print('Invalid number, please try again.')
+            continue
+        if choice == 1:
+            add_task()
+            save_tasks()
+        elif choice == 2:
+            display_tasks()
+        elif choice == 3:
+            remove_task()
+            save_tasks()
+        elif choice == 4:
+            mark_done()
+            save_tasks()
+        elif choice == 5:
+            edit_task()
+            save_tasks()    
+        elif choice == 6:
+            save_tasks()
+            print('Tasks saved. Exiting the programm...')
+            break
+        else:
+            if choice < 1 or choice > 6:
+                print('Invalid number, please try again.')
+                continue
+
+load_tasks()
+print('\nWelcome to the menu, please choose an option:\n')
+show_menu()
